1bf8a7cc4f3a8bea268fce68f229aacf7f9fd2fb
Ismail Abazov <41200208+ismabazoff@users.noreply.github.com>
2023-12-26 12:22:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     root: './src',
     build: {
+        minify: false, // отключает сжатие файлов при сборке
         outDir: './../dist',
         emptyOutDir: true,
         rollupOptions: {
